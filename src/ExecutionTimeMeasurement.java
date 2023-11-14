b42fa8bd030931a7a799f9558c322d4d45975a98
Dohyun Lee <135339790+Don-Cass@users.noreply.github.com>
2023-11-14 23:06:25 +0900

    Add files via upload
    난수 생성으로 인한 시간 지연 픽스

--- src/ExecutionTimeMeasurement.java
+++ src/ExecutionTimeMeasurement.java
@@ -2,32 +2,41 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class ExecutionTimeMeasurement {
-    public static final int MAX_SIZE = 1000000;
-    public static final int MAX_VALUE = 1000000;
-    public static final int Number_of_implementations = 100000;
+    public static final int MAX_SIZE = 10000000;
+    public static final int MAX_VALUE = 10000000;
+    public static final int Number_of_implementations = 10000;
     public static void main(String[] args) {
 
         try (
-                FileWriter writer1 = new FileWriter("execution_times_array.csv");
-                FileWriter writer2 = new FileWriter("execution_times_heap.csv")
+                FileWriter writer1 = new FileWriter("execution_times_array2.csv");
+                FileWriter writer2 = new FileWriter("execution_times_heap2.csv")
         ) {
-            writer1.write("Run,Size,Time_insert (nanoseconds),Time_getMax (nanoseconds),Time_deleteMax (nanoseconds) \n");
-            writer2.write("Run,Size,Time_insert (nanoseconds),Time_getMax (nanoseconds),Time_deleteMax (nanoseconds) \n");
-            for (int i = 0; i <= Number_of_implementations; i++) {
+            writer1.write("Run,Size,Time_insert (nanoseconds)\n");//Time_getMax (nanoseconds),Time_deleteMax (nanoseconds) \n");
+            writer2.write("Run,Size,Time_insert (nanoseconds)\n");//Time_getMax (nanoseconds),Time_deleteMax (nanoseconds) \n");
+//            for (int i = 0; i <= Number_of_implementations; i++) {
+//
+//                int randomSize;
+//                randomSize = (int)(Math.random()* MAX_SIZE)+1;
+//                MyArray ma = new MyArray(randomSize);
+//                MyHeap mh = new MyHeap(randomSize);
+            for (int i = 0; i < Number_of_implementations; i++) {
+                int randomSize = (int)(Math.random() * MAX_SIZE) + 1;
+                int[] randomValues = new int[randomSize];
 
-                int randomSize;
-                randomSize = (int)(Math.random()* MAX_SIZE)+1;
+                // 난수 값을 미리 생성
+                for (int j = 0; j < randomSize; j++) {
+                    randomValues[j] = (int)(Math.random() * MAX_VALUE);
+                }
+
                 MyArray ma = new MyArray(randomSize);
                 MyHeap mh = new MyHeap(randomSize);
-
                 /////////////////////array 실험
 
                 //////////배열 자료의 삽입 실험
                 long startTime_insert = System.nanoTime();
 
                 for (int j = 0; j < randomSize; j++) {
-                    int randomValue = (int)(Math.random() * MAX_VALUE);
-                    ma.insert( randomValue ); // 0~MAX_VALUE 까지 임의
+                    ma.insert(randomValues[j]); // 미리 생성된 난수 값 사용
                 }
 
                 long endTime_insert = System.nanoTime();
@@ -35,20 +44,20 @@ public class ExecutionTimeMeasurement {
 
 
                 //////////배열 최댓값 확인 실험
-                long startTime_getMax = System.nanoTime();
-
-                int max = ma.getMax();
-
-                long endTime_getMax = System.nanoTime();
-
-
-
-                //////////배열 최댓값 삭제 실험
-                long startTime_deleteMax = System.nanoTime();
-
-                ma.deleteMax();
-
-                long endTime_deleteMax = System.nanoTime();
+//                long startTime_getMax = System.nanoTime();
+//
+//                int max = ma.getMax();
+//
+//                long endTime_getMax = System.nanoTime();
+//
+//
+//
+//                //////////배열 최댓값 삭제 실험
+//                long startTime_deleteMax = System.nanoTime();
+//
+//                ma.deleteMax();
+//
+//                long endTime_deleteMax = System.nanoTime();
 
                 /////////////////////heap 실험
 
@@ -56,38 +65,37 @@ public class ExecutionTimeMeasurement {
                 long startTime_insertHeap = System.nanoTime();
 
                 for (int j = 0; j < randomSize; j++) {
-                    int randomValue = (int)(Math.random() * MAX_VALUE);
-                    mh.insertHeap( randomValue ); // 0~MAX_VALUE 까지 임의
+                    mh.insertHeap(randomValues[j]); // 미리 생성된 난수 값 사용
                 }
 
                 long endTime_insertHeap = System.nanoTime();
 
                 //////////Heap 최댓값 출력 실험
 
-                long startTime_peekHeap = System.nanoTime();
-
-                max = mh.peekHeap();
-
-                long endTime_peekHeap = System.nanoTime();
-
-                //////////Heap 최댓값 삭제 실험
-
-                long startTime_getMaxHeap = System.nanoTime();
-
-                max = mh.getMaxHeap();
-
-                long endTime_getMaxHeap = System.nanoTime();
+//                long startTime_peekHeap = System.nanoTime();
+//
+//                max = mh.peekHeap();
+//
+//                long endTime_peekHeap = System.nanoTime();
+//
+//                //////////Heap 최댓값 삭제 실험
+//
+//                long startTime_getMaxHeap = System.nanoTime();
+//
+//                max = mh.getMaxHeap();
+//
+//                long endTime_getMaxHeap = System.nanoTime();
 
 
                 long executionTime_insert = endTime_insert - startTime_insert;
-                long executionTime_getMax = endTime_getMax - startTime_getMax;
-                long executionTime_deleteMax = endTime_deleteMax - startTime_deleteMax;
+//                long executionTime_getMax = endTime_getMax - startTime_getMax;
+//                long executionTime_deleteMax = endTime_deleteMax - startTime_deleteMax;
                 long executionTime_insertHeap = endTime_insertHeap - startTime_insertHeap;
-                long executionTime_peekHeap = endTime_peekHeap - startTime_peekHeap;
-                long executionTime_getMaxHeap = endTime_getMaxHeap - startTime_getMaxHeap;
+//                long executionTime_peekHeap = endTime_peekHeap - startTime_peekHeap;
+//                long executionTime_getMaxHeap = endTime_getMaxHeap - startTime_getMaxHeap;
 
-                writer1.write(i + "," + randomSize + "," + executionTime_insert + "," + executionTime_getMax + "," + executionTime_deleteMax + "\n");
-                writer2.write(i + "," + randomSize + "," + executionTime_insertHeap + "," + executionTime_peekHeap + "," + executionTime_getMaxHeap + "\n");
+                writer1.write(i + "," + randomSize + "," + executionTime_insert + "\n"); // + "," + executionTime_getMax + "," + executionTime_deleteMax + "\n");
+                writer2.write(i + "," + randomSize + "," + executionTime_insertHeap + "\n");// "," + executionTime_peekHeap + "," + executionTime_getMaxHeap + "\n");
             }
         } catch (IOException e) {
             e.printStackTrace();
